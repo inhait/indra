8ba647128e8b2bd695a06d492ca736dcd9c4ce40
d0rkj <0c001null@gmail.com>
2024-02-29 21:35:45 +0100

    init commit

--- indra.rb
+++ indra.rb
@@ -1,14 +1,2 @@
+
 require 'socket'
-
-def Checking(port)
-    remote_add  = Socket.sockaddr_in(port, 'www.example.com')
-    begin
-        socket.connect_nonblock(remote_addr)
-    rescue Errno:EINPROGRESS
-    end
-end
-lstport = [21,22,23,25,80,8080,443,441]
-threads = []
-lstport.each { |i| threads << Thread.new { scan_port(i) }}
-end
-
