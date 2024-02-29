1e7cd52e465335e23e8d0cf04e2a8d2fbe9bef3f
d0rkj <0c001null@gmail.com>
2024-02-29 21:34:57 +0100

    init commit

--- indra.rb
+++ indra.rb
@@ -6,12 +6,6 @@ def Checking(port)
         socket.connect_nonblock(remote_addr)
     rescue Errno:EINPROGRESS
     end
-    _, sockets, _ = IO.select(nil [socket], nil, TIMEOUT)
-
-    if sockets
-        p "| port: #{port} is open "
-    else
-        
 end
 lstport = [21,22,23,25,80,8080,443,441]
 threads = []
